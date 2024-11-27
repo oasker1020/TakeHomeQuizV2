f3c5abe462b6c3cab7d8f04019a7d92f3b18d97b
Omer-W <omermwaseem@gmail.com>
2024-11-26 21:57:42 -0500

    Added lamda
    slightly adjusted code to work with lamda expression

--- TakeHomeCode.java
+++ TakeHomeCode.java
@@ -2,19 +2,17 @@
 public interface AddNumbers {
     int add(int start, int end);
 }
-public class TakeHomeCode{
-	public static void main(String[] args){
-        AddNumbers adder = new AddNumbers() {
-            @Override
-            public int add(int start, int end) {
-                int sum = 0;
-                for (int i = start; i <= end; i++) {
-                    sum += i;
-                }
-                return sum;
+
+public class TakeHomeCode {
+    public static void main(String[] args) {
+        AddNumbers adder = (start, end) -> {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                sum += i;
             }
+            return sum;
         };
         int result = adder.add(1, 5);
         System.out.println("Sum of numbers from 1 to 5: " + result);
-	}
+    }
 }
