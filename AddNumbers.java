a18ce4dd71086702872b0ea94b2aafc3fdd94f6c
Omer-W <omermwaseem@gmail.com>
2024-11-27 14:08:30 -0500

    CMD Arguments/Split
    Split AddNumbers interface into its own file. Also altered code slightly so that command line arguments can be inputed.

---
+++ AddNumbers.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+public interface AddNumbers 
+{
+    int add(int start, int end);
+}
--- TakeHomeCode.java
+++ TakeHomeCode.java
@@ -1,18 +1,24 @@
-@FunctionalInterface
-public interface AddNumbers {
-    int add(int start, int end);
-}
-
 public class TakeHomeCode {
     public static void main(String[] args) {
-        AddNumbers adder = (start, end) -> {
-            int sum = 0;
-            for (int i = start; i <= end; i++) {
-                sum += i;
-            }
-            return sum;
-        };
-        int result = adder.add(1, 5);
-        System.out.println("Sum of numbers from 1 to 5: " + result);
+        if (args.length != 2) {
+            System.out.println("Please enter exactly two arguments.");
+            return;
+        }
+        try {
+            int start = Integer.parseInt(args[0]);
+            int end = Integer.parseInt(args[1]);
+            AddNumbers adder = (a, b) -> {
+                int sum = 0;
+                for (int i = start; i <= end; i++) {
+                    sum += i;
+                }
+                return sum;
+            };
+            int result = adder.add(1, 5);
+            System.out.println("Sum of numbers from 1 to 5: " + result);
+        }catch(NumberFormatException e)
+        {
+            System.out.println("Invalid input. Please provide two integers.");
+        }
     }
 }
